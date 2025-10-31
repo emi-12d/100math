d1ce81c634467014189ce3bd0964583f4e1b1afc
kit0301 <kanon20040303@gmail.com>
2025-10-31 14:19:11 +0900

    main.pyの桁数の引数の追加

--- main.py
+++ main.py
@@ -9,14 +9,16 @@ parser = argparse.ArgumentParser(description="100マス計算")
 # 必須の位置引数を2つ定義
 # これらは指定しないと自動でエラーになる
 parser.add_argument("operator", help="演算子 (+, -, *, /) を指定")
-parser.add_argument("digits", type=int, help="計算に使う数字の桁数を指定")
+parser.add_argument("left_digits", type=int, help="左側の数字の桁数を指定")
+parser.add_argument("top_digits", type=int, help="上側の数字の桁数を指定")
 
 # 引数をパース
 # ここで引数が足りなければ、argparseが自動でエラーを出して終了する
 args = parser.parse_args()
 
 operator = args.operator
-digits = args.digits
+left_digits = args.left_digits
+top_digits = args.top_digits
 
 # 念のため、演算子の種類をチェックする処理を追加
 allowed_operators = ["+", "-", "*", "/"]
@@ -26,7 +28,7 @@ if operator not in allowed_operators:
     sys.exit(1)  # エラーで終了
 
 # 桁数が5以上の場合にエラーを出す
-if digits >= 5:
+if left_digits >= 5 or top_digits >= 5:
     print(
         'エラー: 4桁までの整数のみに対応しています。\ndigitには4以下の数値を入力してください。\nExample : `python main.py "+" 4`',
         file=sys.stderr,
@@ -34,6 +36,6 @@ if digits >= 5:
     sys.exit(1)  # エラーで終了
 
 print(f"演算子: {operator}")
-print(f"桁数: {digits}")
+print(f"桁数: {left_digits, top_digits}")
 
-generator.generate_pdf(operator, digits)
+generator.generate_asymmetric_number_of_characters_Hundred_Square_Calculations_pdf(operator, left_digits, top_digits)
